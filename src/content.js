ef6bd9f61dc998771624e177e5874c37f7c5cdb5
Laurent Cozic <laurent22@users.noreply.github.com>
2023-11-04 10:39:20 +0000

    Handle tabs in output, and made bg colour darker

--- src/content.js
+++ src/content.js
@@ -1,13 +1,18 @@
 const Convert = require('ansi-to-html');
 
 const color = '#d1d7dd';
-const bgColor = '#25292e';
+const bgColor = '#151516';
 
 const convert = new Convert();
 
 const ansiToHtml = (ansi) => {
 	const lines = ansi.split('\n');
-	const converted = lines.map(l => convert.toHtml(l));
+	const converted = lines.map(l => {
+		// We also replace consecutive spaces by `&nbsp;` otherwise they won't
+		// be displayed.
+		return convert.toHtml(l).replace(/ {2,}/g, match => '&nbsp;'.repeat(match.length));
+	});
+	
 	return '<div style="background-color: ' + bgColor + '; color: ' + color + '; font-family: monospace; white-space: nowrap;">' + converted.join('<br/>') + '</div>';
 }
 
